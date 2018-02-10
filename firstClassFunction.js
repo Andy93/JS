06f17afe172b0f0f15272e4fdabe3ba42a64e63a
Andy93 <oneillandrew101@gmail.com>
2018-02-10 14:55:38 +0000

    Update firstClassFunction.js

--- firstClassFunction.js
+++ firstClassFunction.js
@@ -1,3 +1,29 @@
+// First Class Funtions : Funtions as Arguments
+
+var numbers = [20, 40, 60, 80, 100];
+
+function arrayFunc(arr, fn){
+    arrRes = [];
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function numberMinus(x){
+    return 100 - x;
+}
+
+function compareNumber(x){
+    return x >=40;
+}
+
+var newNumbers = arrayFunc(numbers, numberMinus);
+var numBool = arrayFunc(newNumbers, compareNumber);
+
+console.log(newNumbers);
+console.log(numBool);
+
 // First Class Funtions : Funtions Returning Functions
 
 function question(job){
